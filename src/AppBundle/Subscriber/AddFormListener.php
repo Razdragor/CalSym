a59a07303c59bcd806e601e0c694a7a3a9c29e1c
Julian <julian.michau@gmail.com>
2017-02-06 23:31:07 +0100

    remove cmt

--- src/AppBundle/Subscriber/AddFormListener.php
+++ src/AppBundle/Subscriber/AddFormListener.php
@@ -8,7 +8,6 @@
 
 namespace AppBundle\Subscriber;
 
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -20,7 +19,6 @@ class AddFormListener implements EventSubscriberInterface {
     {
         return array(
             FormEvents::PRE_SET_DATA => 'onPreSetData',
-            FormEvents::PRE_SUBMIT   => 'onPreSubmit',
         );
     }
 
@@ -28,43 +26,14 @@ class AddFormListener implements EventSubscriberInterface {
     {
         $user = $event->getData();
         $form = $event->getForm();
-        if (null != $event->getData()) {
-            var_dump($user->getRole());
-        }
-//        var_dump($user->getId());
 
-
-        // Check whether the user from the initial data has chosen to
-        // display his email or not.
-//        if (true === $user->isGranted()) {
         if (null != $event->getData()) {
             if($user->getRole() == "PROF")
             {
-
                 $form->add('activity', TextType::class);
+                $form->add('address', TextType::class);
             }
         }
-
-//        }
     }
 
-    public function onPreSubmit(FormEvent $event)
-    {
-        $user = $event->getData();
-        $form = $event->getForm();
-
-        if (!$user) {
-            return;
-        }
-
-        // Check whether the user has chosen to display his email or not.
-        // If the data was submitted previously, the additional value that
-        // is included in the request variables needs to be removed.
-        if (true === $user['show_email']) {
-            $form->add('email', EmailType::class);
-        } else {
-            unset($user['email']);
-            $event->setData($user);
-        }
-    }
 }
